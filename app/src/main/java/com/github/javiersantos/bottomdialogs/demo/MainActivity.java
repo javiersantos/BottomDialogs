fcdff329156b5a172aaa7d916ae298b9a9bcc5a0
Javier Santos <me@javiersantos.me>
2016-09-23 12:47:50 +0200

    Remove unnecessary imports

--- app/src/main/java/com/github/javiersantos/bottomdialogs/demo/MainActivity.java
+++ app/src/main/java/com/github/javiersantos/bottomdialogs/demo/MainActivity.java
@@ -5,16 +5,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 import com.github.javiersantos.bottomdialogs.BottomDialog;
 import com.mikepenz.iconics.IconicsDrawable;
